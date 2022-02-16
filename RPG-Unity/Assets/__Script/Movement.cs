d3f0e76b2155fb9198bd67bccbf153deb4dfa047
golivott <golivott@uwo.ca>
2022-02-16 14:02:41 -0500

    Added Character Sprite

---
+++ RPG-Unity/Assets/__Script/Movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10;
+    void FixedUpdate()
+    {
+
+        // Calculating move direction
+        Vector2 moveDir = Vector2.zero;
+        
+        if (Input.GetKey(KeyCode.W))
+            moveDir.y = 1;
+        if (Input.GetKey(KeyCode.S))
+            moveDir.y = -1;
+        if (Input.GetKey(KeyCode.D))
+            moveDir.x = 1;
+        if (Input.GetKey(KeyCode.A))
+            moveDir.x = -1;   
+
+        // Moving Character
+        gameObject.GetComponent<Rigidbody2D>().velocity = moveDir * speed * Time.fixedDeltaTime;
+    }
+}
