33afe4c6b6705019920576a0dbaf39eefabc3cd5
golivott <golivott@uwo.ca>
2022-03-21 18:45:50 -0400

    added attack sprites

--- RPG-Unity/Assets/__Script/Attack.cs
+++ RPG-Unity/Assets/__Script/Attack.cs
@@ -7,16 +7,21 @@ public class Attack : MonoBehaviour
 {
     public float attackDist = 1f;
     public float attackRange = 1f;
-
-    public Vector2 attackPoint;
+    
     public LayerMask enemyLayers;
+    public GameObject swipeAttack;
+    
+    [Header("Set Dynamically")]
+    public Vector2 attackPoint;
+    public Vector2 lastMoveDir;    
 
     void Update()
     {
         // Updating attack point
-        if (transform.GetComponent<Movement>().moveDir != Vector2.zero)
+        if (GetComponent<Movement>().moveDir != Vector2.zero)
         {
-            attackPoint = transform.GetComponent<Movement>().moveDir * attackDist + new Vector2(transform.position.x, transform.position.y);
+            lastMoveDir = GetComponent<Movement>().moveDir;
+            attackPoint = GetComponent<Movement>().moveDir * attackDist + new Vector2(transform.position.x, transform.position.y);
         }
 
         if (Input.GetKey(KeyCode.Space))
@@ -28,6 +33,11 @@ public class Attack : MonoBehaviour
     void Attack1()
     {
         // Show attack sprite
+        GameObject attackSprite = swipeAttack;
+        attackSprite.transform.position = attackPoint;
+        attackSprite.transform.rotation = Quaternion.EulerAngles(0,0,(float)Math.Tan(Convert.ToDouble(lastMoveDir.y/lastMoveDir.x)));
+        Instantiate(attackSprite);
+        Destroy(attackSprite,0.5f);
 
         // Gets enemys hit by attack
         Collider2D[] enemyHits = Physics2D.OverlapCircleAll(attackPoint, attackRange, enemyLayers);
@@ -37,5 +47,10 @@ public class Attack : MonoBehaviour
         {
             print("hit: " + enemy);
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint,attackRange);
     }
 }
