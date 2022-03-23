90fe094163bd0780d420f186063d9b9916812d48
golivott <golivott@uwo.ca>
2022-03-22 21:47:48 -0400

    commenting attack scripts

--- RPG-Unity/Assets/__Script/ProjectileAttack.cs
+++ RPG-Unity/Assets/__Script/ProjectileAttack.cs
@@ -5,27 +5,24 @@ using UnityEngine;
 
 public class ProjectileAttack : MonoBehaviour
 {
-    private List<Collider2D> _colliders;
+    // Some variables to set how the projectile should move and act
     private float _damage = 50f;
     private float _spin = 0f;
     private bool _followPlayer = false;
     private float _growth = 0f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        _colliders = new List<Collider2D>(); 
-    }
 
     private void FixedUpdate()
     {
+        // Apply rotation
         transform.Rotate(0,0,_spin);
 
+        // Apply Growth
         if (_growth != 0)
         {
             transform.localScale *= _growth;
         }
-
+        
+        // if it should follow the player
         if (_followPlayer)
         {
             transform.position = GameObject.FindWithTag("Player").gameObject.transform.position;
@@ -34,27 +31,32 @@ public class ProjectileAttack : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        // Apply damage to the enemy
         if (col.gameObject.tag.Equals("Enemy"))
         {
             col.gameObject.GetComponent<Enemy>().TakeDamage(_damage);
         }
     }
 
+    // Sets damage value
     public void SetDamage(float damage)
     {
         _damage = damage;
     }
     
+    // Sets spin value
     public void SetSpin(float spin)
     {
         _spin = spin;
     }
 
+    // Sets if the attack should follow the player
     public void IsFollowPlayer()
     {
         _followPlayer = true;
     }
 
+    // Sets if the attack should grow in size
     public void SetGrowth(float growth)
     {
         _growth = growth;
